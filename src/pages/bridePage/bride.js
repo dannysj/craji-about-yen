95c8c8e1a1f0e28c45d0419b241d0128720a8db1
Danny Chew <dannychew@Dannys-MacBook.local>
2024-04-07 23:53:09 -0700

    fixed

--- src/pages/bridePage/bride.js
+++ src/pages/bridePage/bride.js
@@ -3,7 +3,8 @@ import config from '../../config/AssetPath';
 import Introduction from '../introduction/Introduction';
 import InvitationDetails from '../invitationDetails/InvitationDetails';
 import RSVPDetails from '../rsvpDetails/RSVPDetails';
-import { useEffect, CSSProperties } from 'react';
+import { useEffect } from 'react';
+import PlayButton from '../../components/playButton/PlayButton';
 
 function BridePage() {
   useEffect(() => {
@@ -15,7 +16,7 @@ function BridePage() {
     <>
       <style>{config.globalStyles()}</style>
       <div className="App">
-        <PlayButton audioUrl={"ipoh"} />
+        <PlayButton audioUrl={"ipoh"}   />
         <Introduction imageVar={config.IMAGES.KEY.FIRST_BRIDE}/>
         <InvitationDetails overrideStyle={invitationStyleOverride} targetDateStr={config.CONFIGURATION.BRIDE.TARGET_DATE} imageVar={config.IMAGES.KEY.SECOND} venueName={config.CONFIGURATION.BRIDE.VENUE} venueAddress={config.CONFIGURATION.BRIDE.VENUE_ADDRESS}/>
         <RSVPDetails rsvpLink={config.CONFIGURATION.BRIDE.RSVP_LINK} imageVar={config.IMAGES.KEY.THIRD}/>
