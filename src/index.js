30b958bd3e33a0683b1c05ad65d790dc8d0b3329
Danny Chew <dannychew@Dannys-MacBook.local>
2024-04-12 21:43:05 -0700

    put back

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/craji-for-yen">
     <React.StrictMode>
       <App />
     </React.StrictMode>
